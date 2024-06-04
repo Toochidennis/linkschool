57f620b38c29cfb91ab18d29f16d0fbd90275964
Toochidennis <dennistoochukwu@gmail.com>
2024-06-04 14:14:22 +0100

    update

---
+++ lib/ui/dashboard/common/common/bottom_navigation_bar.dart
@@ -0,0 +1,103 @@
+import 'package:curved_nav_bar/curved_bar/curved_action_bar.dart';
+import 'package:curved_nav_bar/fab_bar/fab_bottom_app_bar_item.dart';
+import 'package:curved_nav_bar/flutter_curved_bottom_nav_bar.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+import '../../../../common/app_colors.dart';
+
+class CustomNavigationBar extends StatefulWidget {
+  final String actionButtonImagePath;
+  final List<FABBottomAppBarItem> appBarItems;
+  final List<Widget> bodyItems;
+
+  const CustomNavigationBar(
+      {super.key,
+      required this.actionButtonImagePath,
+      required this.appBarItems,
+      required this.bodyItems});
+
+  @override
+  State<CustomNavigationBar> createState() => _CustomNavigationBarState();
+}
+
+class _CustomNavigationBarState extends State<CustomNavigationBar> with WidgetsBindingObserver {
+  Brightness? brightness;
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addObserver(this);
+    _updateBrightness();
+  }
+
+  @override
+  void didChangePlatformBrightness() {
+    super.didChangePlatformBrightness();
+    _updateBrightness();
+  }
+
+  void _updateBrightness() {
+    setState(() {
+      brightness = WidgetsBinding.instance.window.platformBrightness;
+    });
+  }
+
+  @override
+  void dispose() {
+    WidgetsBinding.instance.removeObserver(this);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+                  child: CurvedNavBar(
+            actionButton: CurvedActionBar(
+              onTab: (value) => print(value),
+              activeIcon: Container(
+                padding: const EdgeInsets.all(24.0),
+                decoration: BoxDecoration(
+                  color: AppColors.primaryLight,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.secondaryLight,
+                    width: 1.0,
+                  ),
+                ),
+                child: SvgPicture.asset(
+                  widget.actionButtonImagePath,
+                ),
+              ),
+              inActiveIcon: Container(
+                padding: const EdgeInsets.all(24.0),
+                decoration: BoxDecoration(
+                  color: AppColors.primaryLight,
+                  shape: BoxShape.circle,
+                  border: Border.all(
+                    color: AppColors.secondaryLight,
+                    width: 1.0,
+                  ),
+                ),
+                child: SvgPicture.asset(
+                  widget.actionButtonImagePath,
+                ),
+              ),
+            ),
+            activeColor: AppColors.secondaryLight,
+            inActiveColor: AppColors.primaryLight,
+            navBarBackgroundColor: brightness == Brightness.light
+                ? AppColors.backgroundLight
+                : AppColors.backgroundDark,
+            appBarItems: widget.appBarItems,
+            bodyItems: widget.bodyItems,
+                  ),
+                ),
+          ],
+        ));
+  }
+}
---
+++ lib/ui/dashboard/explore/create_bottom_nar_icon.dart
@@ -0,0 +1,34 @@
+import 'package:curved_nav_bar/fab_bar/fab_bottom_app_bar_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import '../../../common/app_colors.dart';
+
+FABBottomAppBarItem createBottomNavIcon({
+  required String imagePath,
+  required String text,
+  double? height,
+  double? width,
+}) {
+  return FABBottomAppBarItem(
+    activeIcon: SvgPicture.asset(
+      imagePath,
+      colorFilter: const ColorFilter.mode(
+        AppColors.secondaryLight,
+        BlendMode.srcIn,
+      ),
+      height: height ?? 24.0,
+      width: width ?? 24.0,
+    ),
+    inActiveIcon: SvgPicture.asset(
+      imagePath,
+      colorFilter: const ColorFilter.mode(
+        AppColors.primaryLight,
+        BlendMode.srcIn,
+      ),
+      height: height ?? 24.0,
+      width: width ?? 24.0,
+    ),
+    text: text,
+  );
+}
