2eb0c0ac9097fab8da8ffcd013065ce41b347c08
Toochidennis <dennistoochukwu@gmail.com>
2024-06-20 22:31:05 +0100

    update

--- lib/ui/games/games_home.dart
+++ lib/ui/games/games_home.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:linkschool/common/constants.dart';
@@ -13,87 +14,88 @@ class GamesHome extends StatefulWidget {
 }
 
 class _GamesHomeState extends State<GamesHome> {
-  final PageController _pageController = PageController(viewportFraction: 1);
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: Constants.customAppBar(context),
       body: Container(
         decoration: Constants.customBoxDecoration(context),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Trending now',
-                style: AppTextStyles.normal500(
-                  fontSize: 20.0,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            const SizedBox(height: 12.0),
-            SizedBox(
-              height: 200,
-              child: ListView(
-                padding: EdgeInsets.only(right: 16.0),
-                scrollDirection: Axis.horizontal,
-                children: [
-                  _buildTrendingCard(
-                    startColor: AppColors.gamesColor1,
-                    endColor: AppColors.gamesColor2,
-                    imagePath: 'assets/images/games_1.png',
-                    gameName: 'Overwatch',
-                    platform: 'Cross-platform',
-                    rating: 2.5,
-                  ),
-                  _buildTrendingCard(
-                    startColor: AppColors.gamesColor3,
-                    endColor: AppColors.gamesColor4,
-                    imagePath: 'assets/images/games_2.png',
-                    gameName: 'Borderlands 2',
-                    platform: 'Cross-platform',
-                    rating: 3.5,
-                  ),
-                  _buildTrendingCard(
-                    startColor: AppColors.gamesColor5,
-                    endColor: AppColors.gamesColor6,
-                    imagePath: 'assets/images/games_3.png',
-                    gameName: 'Borderlands 2',
-                    platform: 'Cross-platform',
-                    rating: 3.5,
-                  ),
-                ],
-              ),
-            ),
-            const SizedBox(height: 16.0),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16.0),
-              child: Text(
-                'Suggested for you',
-                style: AppTextStyles.normal500(
-                  fontSize: 20.0,
-                  color: Colors.black,
-                ),
-              ),
-            ),
-            SizedBox(height: 12.0),
-            SizedBox(
-                height: 300.0,
-                child: PageView(
-                  padEnds: false,
-                  controller: _pageController,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              _buildHeading(title: "Trending now"),
+              const SizedBox(height: 10.0),
+              SizedBox(
+                height: 220,
+                child: ListView(
+                  padding: const EdgeInsets.only(right: 16.0),
                   scrollDirection: Axis.horizontal,
-                  scrollBehavior: ScrollBehavior(),
                   children: [
-                    _buildSuggestedCard(),
-                    _buildSuggestedCard(),
+                    _buildTrendingCard(
+                      startColor: AppColors.gamesColor1,
+                      endColor: AppColors.gamesColor2,
+                      imagePath: 'assets/images/games_1.png',
+                      gameName: 'Overwatch',
+                      platform: 'Cross-platform',
+                      rating: 2.5,
+                    ),
+                    _buildTrendingCard(
+                      startColor: AppColors.gamesColor3,
+                      endColor: AppColors.gamesColor4,
+                      imagePath: 'assets/images/games_2.png',
+                      gameName: 'Borderlands 2',
+                      platform: 'Cross-platform',
+                      rating: 3.5,
+                    ),
+                    _buildTrendingCard(
+                      startColor: AppColors.gamesColor5,
+                      endColor: AppColors.gamesColor6,
+                      imagePath: 'assets/images/games_3.png',
+                      gameName: 'Borderlands 2',
+                      platform: 'Cross-platform',
+                      rating: 3.5,
+                    ),
                   ],
-                ))
-          ],
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              _buildHeading(title: "Suggested for you"),
+              const SizedBox(height: 10.0),
+              CarouselSlider(
+                items: [
+                  _buildSuggestedCard(left: 16.0),
+                  _buildSuggestedCard(),
+                  _buildSuggestedCard(right: 16.0),
+                ],
+                options: CarouselOptions(
+                  height: 280.0,
+                  padEnds: false,
+                  viewportFraction: 0.95,
+                  autoPlay: true,
+                  enableInfiniteScroll: false,
+                  scrollDirection: Axis.horizontal,
+                ),
+              ),
+              const SizedBox(height: 16.0),
+              _buildHeading(title: "You may like"),
+              const SizedBox(height: 10.0),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildHeading({required String title}) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Text(
+        title,
+        style: AppTextStyles.normal500(
+          fontSize: 20.0,
+          color: Colors.black,
         ),
       ),
     );
@@ -165,21 +167,25 @@ class _GamesHomeState extends State<GamesHome> {
     );
   }
 
-  Widget _buildSuggestedCard() {
+  Widget _buildSuggestedCard({
+    double? left,
+    double? right,
+  }) {
     return Padding(
-      padding: const EdgeInsets.only(left: 16.0),
+      padding: EdgeInsets.only(left: left ?? 10.0, right: right ?? 0.0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           ClipRRect(
-            borderRadius: BorderRadius.circular(10.0), // Add borderRadius for ClipRRect
+            borderRadius: BorderRadius.circular(10.0),
+            // Add borderRadius for ClipRRect
             child: Image.asset(
               'assets/images/millionaire.png',
               fit: BoxFit.cover,
               height: 200,
             ),
           ),
-          const SizedBox(height: 16.0),
+          const SizedBox(height: 12.0),
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: [
@@ -197,7 +203,7 @@ class _GamesHomeState extends State<GamesHome> {
                 ],
               ),
               Container(
-                height: 50.0,
+                height: 45.0,
                 decoration: BoxDecoration(
                   gradient: const LinearGradient(
                     begin: Alignment.topLeft,
@@ -231,6 +237,44 @@ class _GamesHomeState extends State<GamesHome> {
                 ),
               ),
             ],
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildYouMayLikeCard({
+    required Color startColor,
+    required Color endColor,
+    required String imagePath,
+    required String gameName,
+    required String platform,
+    required double rating,
+    required int downloadCount,
+  }) {
+    return Padding(
+      padding: EdgeInsets.all(10.0),
+      child: Row(
+        children: [
+          Container(
+            margin: const EdgeInsets.only(left: 16.0),
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+            width: 125.0,
+            height: 125.0,
+            decoration: BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  colors: [
+                    startColor,
+                    endColor,
+                  ],
+                ),
+                borderRadius: BorderRadius.circular(16.0)),
+            child: Image.asset(
+              imagePath,
+              fit: BoxFit.contain,
+            ),
           ),
         ],
       ),
