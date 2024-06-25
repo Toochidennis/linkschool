1fcca980464e8a1937acbab976f179284be8e356
Toochidennis <dennistoochukwu@gmail.com>
2024-06-25 17:10:32 +0100

    update 25/06/2024

--- lib/ui/books/books_home.dart
+++ lib/ui/books/books_home.dart
@@ -1,4 +1,8 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:linkschool/common/search_bar.dart';
+import 'package:linkschool/common/text_styles.dart';
 import 'package:linkschool/ui/books/books_button_item.dart';
 import 'package:linkschool/ui/books/custom_tab_controller.dart';
 
@@ -15,9 +19,9 @@ class BooksHome extends StatefulWidget {
 //TickerProviderStateMixin
 
 class _BooksHomeState extends State<BooksHome> {
-  int _selectedButtonIndex = 0;
+  int _selectedBookCategoriesIndex = 0;
 
-  final buttonLabels = [
+  final bookCategories = [
     'Academic',
     'Science Fiction',
     'For WAEC',
@@ -31,76 +35,44 @@ class _BooksHomeState extends State<BooksHome> {
     return Scaffold(
       appBar: Constants.customAppBar(context: context),
       body: Container(
-        height: double.infinity,
         decoration: Constants.customBoxDecoration(context),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Padding(
-                padding: EdgeInsets.symmetric(horizontal: 16.0),
-                child: Text(
-                  'What do you want to\nread today?',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 24,
-                    fontWeight: FontWeight.w600,
-                    fontFamily: 'Urbanist',
-                  ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Text(
+                'What do you want to\nread today?',
+                style: AppTextStyles.normal600(
+                  fontSize: 24.0,
+                  color: Colors.black,
                 ),
               ),
-              const Padding(
-                padding: EdgeInsets.all(16.0),
-                child: SizedBox(
-                  height: 50,
-                  child: TextField(
-                    decoration: InputDecoration(
-                      prefixIcon: Icon(Icons.search),
-                      labelText: 'Search',
-                      labelStyle: TextStyle(
-                        fontSize: 14.0,
-                        color: Color.fromRGBO(139, 139, 139, 1),
-                        fontWeight: FontWeight.w500,
-                      ),
-                      filled: true,
-                      fillColor: AppColors.textFieldLight,
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.all(
-                          Radius.circular(20.0),
-                        ),
-                        gapPadding: 4.0,
-                      ),
-                    ),
-                  ),
-                ),
+            ),
+            const CustomSearchBar(),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              child: Wrap(
+                spacing: 10.0,
+                runSpacing: 10.0,
+                children: List.generate(bookCategories.length, (index) {
+                  return BooksButtonItem(
+                    text: bookCategories[index],
+                    isSelected: _selectedBookCategoriesIndex == index,
+                    onPressed: () {
+                      setState(() {
+                        _selectedBookCategoriesIndex = index;
+                      });
+                    },
+                  );
+                }),
               ),
-              Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16.0),
-                child: Wrap(
-                  spacing: 10.0,
-                  runSpacing: 10.0,
-                  children: List.generate(buttonLabels.length, (index) {
-                    return BooksButtonItem(
-                      text: buttonLabels[index],
-                      isSelected: _selectedButtonIndex == index,
-                      onPressed: () {
-                        setState(() {
-                          _selectedButtonIndex = index;
-                        });
-                      },
-                    );
-                  }),
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              const SizedBox(
-                width: double.maxFinite,
-                height: 1024,
-                child: CustomTabController(),
-              )
-            ],
-          ),
+            ),
+            const Expanded(
+              child: CustomTabController()
+            ),
+          ],
         ),
       ),
     );
