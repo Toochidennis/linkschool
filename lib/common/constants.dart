2475f0e7d696845b41fc5edc17cef76d12a206f2
Toochidennis <dennistoochukwu@gmail.com>
2024-06-25 15:55:03 +0100

    update 25/06/2024

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:linkschool/common/text_styles.dart';
 
 import 'app_colors.dart';
 
@@ -16,7 +17,10 @@ class Constants {
     );
   }
 
-  static AppBar customAppBar(BuildContext context) {
+  static AppBar customAppBar({
+    required BuildContext context,
+    String? iconPath,
+  }) {
     final Brightness brightness = Theme.of(context).brightness;
     var opacity = brightness == Brightness.light ? 0.1 : 0.15;
 
@@ -46,6 +50,66 @@ class Constants {
           color: AppColors.primaryLight,
           width: 34.0,
           height: 34.0,
+        ),
+      ),
+      actions: [
+        if (iconPath != null)
+          IconButton(
+            onPressed: () {},
+            icon: Image.asset(
+              iconPath,
+              width: 24.0,
+              height: 24.0,
+            ),
+          ),
+      ],
+    );
+  }
+
+  static Padding headingWithSeeAll600({
+    required String title,
+    double? titleSize,
+    Color? titleColor,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(
+            title,
+            style: AppTextStyles.normal600(
+              fontSize: titleSize ?? 16.0,
+              color: titleColor ?? AppColors.backgroundDark,
+            ),
+          ),
+          TextButton(
+            onPressed: () {},
+            style: TextButton.styleFrom(),
+            child: const Text(
+              'See all',
+              style: TextStyle(
+                decoration: TextDecoration.underline,
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  static Padding heading600({
+    required String title,
+    double? titleSize,
+    Color? titleColor,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      child: Text(
+        title,
+        style: AppTextStyles.normal600(
+          fontSize: titleSize ?? 16.0,
+          color: titleColor ?? AppColors.backgroundDark,
         ),
       ),
     );
