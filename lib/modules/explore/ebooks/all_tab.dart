f29c72f07ad7c77c55c129e7e6e80b7f3c2b8777
Toochidennis <dennistoochukwu@gmail.com>
2024-06-27 09:59:29 +0100

    update 27/06/2024

--- lib/modules/explore/ebooks/all_tab.dart
+++ lib/modules/explore/ebooks/all_tab.dart
@@ -141,4 +141,64 @@ class AllTab extends StatelessWidget {
       ],
     );
   }
+
+  Widget _buildContinueReadingItem({
+    required String coverImage,
+    required String bookTitle,
+    required String authorName,
+    required double bookProgress,
+}){
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, top: 16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 130,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    coverImage,
+                    fit: BoxFit.cover,
+                    height: 180,
+                    width: 130,
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                LinearProgressIndicator(
+                  value: bookProgress,
+                  // Adjust the value (0.5 means 50% progress)
+                  color: AppColors.primaryLight,
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  bookTitle,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyles.normal500(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  authorName,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyles.normal500(
+                    fontSize: 12.0,
+                    color: AppColors.text5Light,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
 }
