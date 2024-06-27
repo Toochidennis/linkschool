f732642e67d32a1e03fc7b2ce733d78f9732bd1c
Toochidennis <dennistoochukwu@gmail.com>
2024-06-27 11:33:45 +0100

    update 27/06/2024

--- lib/modules/explore/ebooks/all_tab.dart
+++ lib/modules/explore/ebooks/all_tab.dart
@@ -1,23 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:linkschool/modules/common/constants.dart';
 
 import '../../common/app_colors.dart';
 import '../../common/text_styles.dart';
-import '../books/all_tab_item_1.dart';
-import 'all_tab_item_2.dart';
 
 class AllTab extends StatelessWidget {
   const AllTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    const historyItems1 = [
-      AllTabItem1(
+    final readingItems = [
+      _buildContinueReadingItem(
         coverImage: 'assets/images/book_1.png',
         bookTitle: 'Purple Hibiscus',
         authorName: 'Chimamanda N. Adichie',
         bookProgress: 0.2,
       ),
-      AllTabItem1(
+      _buildContinueReadingItem(
         coverImage: 'assets/images/book_2.png',
         bookTitle: 'Doom of Aliens',
         authorName: 'K. S. Jenson',
@@ -25,118 +24,99 @@ class AllTab extends StatelessWidget {
       ),
     ];
 
-    const historyItems2 = [
-      AllTabItem2(
+    final suggestedItems = [
+      _buildSuggestedForYouItem(
         coverImage: 'assets/images/book_4.png',
         bookTitle: 'Sugar Girl',
         authorName: 'UBE Reader Boosters',
       ),
-      AllTabItem2(
+      _buildSuggestedForYouItem(
         coverImage: 'assets/images/book_5.png',
         bookTitle: 'Things Fall Apart',
         authorName: 'Chinua Achebe',
       ),
-      AllTabItem2(
+      _buildSuggestedForYouItem(
         coverImage: 'assets/images/book_3.png',
         bookTitle: 'Americanah',
         authorName: 'Chimamanda N. Adichie',
       ),
     ];
 
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
+    return CustomScrollView(
+      physics: const BouncingScrollPhysics(),
+      slivers: [
+        SliverPadding(
           padding: const EdgeInsets.only(left: 16.0, top: 16.0),
-          child: Text(
-            'Continue reading',
-            style: AppTextStyles.normal500(
-              fontSize: 16.0,
-              color: AppColors.booksButtonTextColor,
+          sliver: SliverToBoxAdapter(
+            child: Text(
+              'Continue reading',
+              style: AppTextStyles.normal500(
+                fontSize: 16.0,
+                color: AppColors.booksButtonTextColor,
+              ),
             ),
           ),
         ),
-        Container(
-          height: 260,
-          margin: const EdgeInsets.only(right: 16.0),
-          decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(color: AppColors.text6Light))),
-          child: ListView.builder(
-              itemCount: historyItems1.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return historyItems1[index];
-              }),
-        ),
-        const SizedBox(height: 10.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'Suggested for you',
-                style: AppTextStyles.title3Light,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(),
-                child: const Text(
-                  'See all',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ),
-            ],
+        SliverToBoxAdapter(
+          child: Container(
+            height: 260,
+            margin: const EdgeInsets.only(right: 16.0),
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(color: AppColors.text6Light))),
+            child: ListView.builder(
+                itemCount: readingItems.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return readingItems[index];
+                }),
           ),
         ),
-        Container(
-          height: 250,
-          margin: const EdgeInsets.only(right: 16.0),
-          decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(color: AppColors.text6Light))),
-          child: ListView.builder(
-              itemCount: historyItems2.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return historyItems2[index];
-              }),
-        ),
-        const SizedBox(height: 10.0),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 16.0),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: [
-              const Text(
-                'You might also like',
-                style: AppTextStyles.title3Light,
-              ),
-              TextButton(
-                onPressed: () {},
-                style: TextButton.styleFrom(),
-                child: const Text(
-                  'See all',
-                  style: TextStyle(
-                    decoration: TextDecoration.underline,
-                  ),
-                ),
-              ),
-            ],
+        const SliverToBoxAdapter(child: SizedBox(height: 10.0)),
+        SliverToBoxAdapter(
+          child: Constants.headingWithSeeAll600(
+            title: 'Suggested for you',
+            titleSize: 18.0,
+            titleColor: AppColors.text2Light,
           ),
         ),
-        Container(
-          height: 250,
-          margin: const EdgeInsets.only(right: 16.0),
-          decoration: const BoxDecoration(
-              border: Border(bottom: BorderSide(color: AppColors.text6Light))),
-          child: ListView.builder(
-              itemCount: historyItems2.length,
-              scrollDirection: Axis.horizontal,
-              itemBuilder: (context, index) {
-                return historyItems2[index];
-              }),
+        SliverToBoxAdapter(
+          child: Container(
+            height: 250,
+            margin: const EdgeInsets.only(right: 16.0),
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(color: AppColors.text6Light))),
+            child: ListView.builder(
+                itemCount: suggestedItems.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return suggestedItems[index];
+                }),
+          ),
+        ),
+        const SliverToBoxAdapter(child: SizedBox(height: 10.0)),
+        SliverToBoxAdapter(
+          child: Constants.headingWithSeeAll600(
+            title: 'You might also like',
+            titleSize: 18.0,
+            titleColor: AppColors.text2Light,
+          ),
+        ),
+        SliverToBoxAdapter(
+          child: Container(
+            height: 250,
+            margin: const EdgeInsets.only(right: 16.0),
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(color: AppColors.text6Light))),
+            child: ListView.builder(
+                itemCount: suggestedItems.length,
+                scrollDirection: Axis.horizontal,
+                itemBuilder: (context, index) {
+                  return suggestedItems[index];
+                }),
+          ),
         ),
       ],
     );
@@ -147,7 +127,7 @@ class AllTab extends StatelessWidget {
     required String bookTitle,
     required String authorName,
     required double bookProgress,
-}){
+  }) {
     return Padding(
       padding: const EdgeInsets.only(left: 16.0, top: 16.0),
       child: Row(
@@ -188,6 +168,60 @@ class AllTab extends StatelessWidget {
                 Text(
                   authorName,
                   maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyles.normal500(
+                    fontSize: 12.0,
+                    color: AppColors.text5Light,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildSuggestedForYouItem({
+    required String coverImage,
+    required String bookTitle,
+    required String authorName,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.only(left: 16.0, top: 10.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          SizedBox(
+            width: 130,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.circular(8.0),
+                  child: Image.asset(
+                    coverImage,
+                    fit: BoxFit.cover,
+                    height: 180,
+                    width: 130,
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  bookTitle,
+                  textAlign: TextAlign.center,
+                  maxLines: 1,
+                  overflow: TextOverflow.ellipsis,
+                  style: AppTextStyles.normal500(
+                    fontSize: 14.0,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 4.0),
+                Text(
+                  authorName,
+                  maxLines: 1,
+                  textAlign: TextAlign.center,
                   overflow: TextOverflow.ellipsis,
                   style: AppTextStyles.normal500(
                     fontSize: 12.0,
