54dd170b286a7b46a49459738f3511cd795c0b76
unknown <joe2sure1@gmail.com>
2024-11-10 01:27:15 +0100

    completed the login_screen UI

--- lib/modules/auth/login_screen.dart
+++ lib/modules/auth/login_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:linkschool/modules/common/app_colors.dart';
+import 'package:linkschool/modules/common/constants.dart';
 import 'package:linkschool/modules/explore/home/explore_dashboard.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
@@ -25,6 +26,7 @@ class _LoginScreenState extends State<LoginScreen> {
   bool _obscurePassword = true;
   bool _showUsernameCheck = false;
   bool _showPasswordCheck = false;
+  late double opacity;
 
 void _navigateToExploreDashboard() {
   Navigator.pushReplacement(
@@ -105,6 +107,8 @@ void _navigateToExploreDashboard() {
 
   @override
   Widget build(BuildContext context) {
+    final Brightness brightness = Theme.of(context).brightness;
+    opacity = brightness == Brightness.light ? 0.1 : 0.15;
     return WillPopScope(
       onWillPop: () async {
         _navigateToExploreDashboard();
@@ -144,6 +148,7 @@ void _navigateToExploreDashboard() {
                 topLeft: Radius.circular(30),
                 topRight: Radius.circular(30),
               ),
+              
             ),
             child: SingleChildScrollView(
               child: Padding(
@@ -313,6 +318,7 @@ void _navigateToExploreDashboard() {
                             style: TextStyle(
                               fontSize: 16,
                               fontWeight: FontWeight.w600,
+                              color: AppColors.backgroundLight
                             ),
                           ),
                         ),
@@ -328,121 +334,3 @@ void _navigateToExploreDashboard() {
     );
   }
 }
-// import 'package:flutter/material.dart';
-// import 'package:shared_preferences/shared_preferences.dart';
-
-// class LoginScreen extends StatefulWidget {
-//   final VoidCallback onLoginSuccess;
-
-//   const LoginScreen({Key? key, required this.onLoginSuccess}) : super(key: key);
-
-//   @override
-//   _LoginScreenState createState() => _LoginScreenState();
-// }
-
-// class _LoginScreenState extends State<LoginScreen> {
-//   final _formKey = GlobalKey<FormState>();
-//   String _role = 'student';
-//   String _username = '';
-//   String _password = '';
-
-//   Future<void> _login() async {
-//     if (_formKey.currentState!.validate()) {
-//       _formKey.currentState!.save();
-
-//       bool isValid = false;
-//       switch (_role) {
-//         case 'student':
-//           isValid = _username == 'student123' && _password == 'password123';
-//           break;
-//         case 'staff':
-//           isValid = _username == 'staff123' && _password == 'password123';
-//           break;
-//         case 'admin':
-//           isValid = _username == 'admin123' && _password == 'password123';
-//           break;
-//       }
-
-//       if (isValid) {
-//         final prefs = await SharedPreferences.getInstance();
-//         await prefs.setString('role', _role);
-//         await prefs.setBool('isLoggedIn', true);
-//         widget.onLoginSuccess();
-//       } else {
-//         ScaffoldMessenger.of(context).showSnackBar(
-//           const SnackBar(content: Text('Invalid username or password')),
-//         );
-//       }
-//     }
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       appBar: AppBar(
-//         title: Text('Login'),
-//       ),
-//       body: Padding(
-//         padding: const EdgeInsets.all(16.0),
-//         child: Form(
-//           key: _formKey,
-//           child: Column(
-//             mainAxisAlignment: MainAxisAlignment.center,
-//             children: [
-//               DropdownButtonFormField<String>(
-//                 value: _role,
-//                 onChanged: (value) {
-//                   setState(() {
-//                     _role = value!;
-//                   });
-//                 },
-//                 items: ['student', 'staff', 'admin']
-//                     .map((role) => DropdownMenuItem<String>(
-//                           value: role,
-//                           child: Text(role),
-//                         ))
-//                     .toList(),
-//               ),
-//               SizedBox(height: 16.0),
-//               TextFormField(
-//                 decoration: InputDecoration(
-//                   labelText: 'Username',
-//                 ),
-//                 validator: (value) {
-//                   if (value!.isEmpty) {
-//                     return 'Please enter a username';
-//                   }
-//                   return null;
-//                 },
-//                 onSaved: (value) {
-//                   _username = value!;
-//                 },
-//               ),
-//               SizedBox(height: 16.0),
-//               TextFormField(
-//                 decoration: InputDecoration(
-//                   labelText: 'Password',
-//                 ),
-//                 obscureText: true,
-//                 validator: (value) {
-//                   if (value!.isEmpty) {
-//                     return 'Please enter a password';
-//                   }
-//                   return null;
-//                 },
-//                 onSaved: (value) {
-//                   _password = value!;
-//                 },
-//               ),
-//               SizedBox(height: 16.0),
-//               ElevatedButton(
-//                 onPressed: _login,
-//                 child: Text('Login'),
-//               ),
-//             ],
-//           ),
-//         ),
-//       ),
-//     );
-//   }
-// }
