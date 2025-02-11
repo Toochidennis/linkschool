80e0c3bd3af20be4475456ae33620568bb7ef5ac
unknown <joe2sure1@gmail.com>
2025-02-11 17:55:23 +0100

    login_screen backend setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,73 @@
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:flutter_dotenv/flutter_dotenv.dart';
+// import 'package:linkschool/modules/common/app_themes.dart';
+// import 'package:linkschool/modules/explore/chat/services/openai_service.dart';
+
+// import 'package:linkschool/routes/onboardingScreen.dart';
+// import 'package:provider/provider.dart';
+// // import 'package:linkschool/app_navigation_flow.dart';
+
+// void main() async{
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await dotenv.load(fileName: ".env");
+//   SystemChrome.setSystemUIOverlayStyle(
+//     const SystemUiOverlayStyle(
+//       statusBarColor: Colors.transparent,
+//       statusBarIconBrightness: Brightness.light, // For Android (dark icons)
+//       statusBarBrightness: Brightness.dark, // For iOS (dark icons)
+//     ),
+//   );
+
+//   // Verify API key is loaded
+//   final apiKey = dotenv.env['OPENAI_API_KEY'];
+//   print('Loaded API key: ${apiKey?.substring(0, 10)}...');
+
+//   runApp(
+//     MultiProvider(
+//       providers: [
+//         Provider(
+//           create: (_) => OpenAIService(),
+//         ),
+//       ],
+//       child: const MyApp(),
+//     ),
+//   );
+//   // runApp(const MyApp());
+// }
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:linkschool/modules/common/app_themes.dart';
 import 'package:linkschool/modules/explore/chat/services/openai_service.dart';
-
+import 'package:linkschool/modules/auth/provider/auth_provider.dart';
 import 'package:linkschool/routes/onboardingScreen.dart';
 import 'package:provider/provider.dart';
-// import 'package:linkschool/app_navigation_flow.dart';
 
-void main() async{
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: ".env");
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
-      statusBarIconBrightness: Brightness.light, // For Android (dark icons)
-      statusBarBrightness: Brightness.dark, // For iOS (dark icons)
+      statusBarIconBrightness: Brightness.light,
+      statusBarBrightness: Brightness.dark,
     ),
   );
 
-  // Verify API key is loaded
   final apiKey = dotenv.env['OPENAI_API_KEY'];
   print('Loaded API key: ${apiKey?.substring(0, 10)}...');
 
   runApp(
     MultiProvider(
       providers: [
-        Provider(
-          create: (_) => OpenAIService(),
-        ),
+        Provider(create: (_) => OpenAIService()),
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
       ],
       child: const MyApp(),
     ),
   );
-  // runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
