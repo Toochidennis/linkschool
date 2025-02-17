405c509210567a9b01e437f8b5213e39cc9c5c96
EWO-CHIKAMSO <kamso0663@gmail.com>
2025-02-17 09:12:24 +0100

    new commit

--- lib/services/video.dart
+++ lib/services/video.dart
@@ -7,21 +7,17 @@ class VideoService {
 
 Future<List<Course>> getAllCourse() async {
     try {
-      print('Fetching data from API: $_BaseUrl');
       final response = await http.get(Uri.parse(_BaseUrl));
-      print('Response received with status: ${response.statusCode}');
-
       if (response.statusCode == 200) {
         print('Response Body: ${response.body}');
         List<dynamic> data = json.decode(response.body);
         return data.map((json) => Course.fromJson(json)).toList();
       } else {
-        print('Failed to load videos, Status Code: ${response.statusCode}');
         throw Exception('Failed to load videos ${response.statusCode}');
       }
     } catch (e, stackTrace) {
       print('Error fetching data: $e');
-      print('Stack Trace: $stackTrace');
+     ;
       throw Exception('Failed to load videos $e');
     }
   }
