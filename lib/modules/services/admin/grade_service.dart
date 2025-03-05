d77d5da00f431dc2a4ae42bd41fddb90e17ada91
EWO-CHIKAMSO <kamso0663@gmail.com>
2025-03-05 09:44:34 +0100

    refactor

--- lib/modules/services/admin/grade_service.dart
+++ lib/modules/services/admin/grade_service.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+
 import '../../model/admin/grade _model.dart';
 
 class GradeService {
@@ -51,3 +52,38 @@ class GradeService {
     }
   }
 }
+
+
+
+
+
+
+
+// import 'package:linkschool/modules/model/admin/grade_model.dart';
+
+// class GradeService {
+//   final String baseUrl = 'http://linkskool.com/developmentportal/api/addGrade.php';
+
+//   Future<List<Grade>> getGrades() async {
+//     final response = await http.get(Uri.parse(baseUrl));
+
+//     if (response.statusCode == 200) {
+//       final Map<String, dynamic> data = json.decode(response.body);
+//       final List<dynamic> gradesJson = data['grades'];
+//       return gradesJson.map((json) => Grade.fromJson(json)).toList();
+//     } else {
+//       throw Exception('Failed to load grades');
+//     }
+//   }
+
+//   Future<void> addGrade(String gradeSymbol, String start, String remark) async {
+//     final Uri uri = Uri.parse('$baseUrl?grade_symbol=$gradeSymbol&start=$start&remark=$remark&_db=linkskoo_practice');
+//     final response = await http.get(uri);
+
+//     if (response.statusCode != 200) {
+//       throw Exception('Failed to add grade');
+//     }
+//   }
+// }
+
+
