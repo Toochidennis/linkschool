7a2bdba912e1e03d3a353a721f13dc95fad7fbd5
unknown <joe2sure1@gmail.com>
2025-03-20 17:32:57 +0100

    refactor api modularity

--- lib/main.dart
+++ lib/main.dart
@@ -19,17 +19,24 @@ import 'package:linkschool/modules/providers/explore/subject_provider.dart';
 import 'package:linkschool/modules/services/explore/cbt_service.dart';
 import 'package:linkschool/routes/onboardingScreen.dart';
 import 'package:provider/provider.dart';
+import 'package:linkschool/modules/services/api/service_locator.dart'; // Add this import
 import 'modules/providers/explore/game/game_provider.dart';
-
-
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-    await Hive.initFlutter();
+
+  // Initialize Hive
+  await Hive.initFlutter();
   await Hive.openBox('userData');
   await Hive.openBox('attendance');
+
+  // Load environment variables
   await dotenv.load(fileName: ".env");
 
+  // Set up the service locator
+  setupServiceLocator();
+
+  // Configure system UI
   SystemChrome.setSystemUIOverlayStyle(
     const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -37,25 +44,29 @@ Future<void> main() async {
       statusBarBrightness: Brightness.dark, // For iOS (dark icons)
     ),
   );
-  runApp(MultiProvider(
-    providers: [
-      ChangeNotifierProvider(create: (_) => AuthProvider()),
-      ChangeNotifierProvider(create: (context) => NewsProvider()),
-      ChangeNotifierProvider(create: (context) => SubjectProvider()),
-      ChangeNotifierProvider(create: (_) => CBTProvider(CBTService())),
-      ChangeNotifierProvider(create: (context) => GameProvider()),
-      ChangeNotifierProvider(create: (_) => ExamProvider()),
-      ChangeNotifierProvider(create: (context) => ForYouProvider()),
-      ChangeNotifierProvider(create: (context) => GradeProvider()),
-      ChangeNotifierProvider(create: (context) => LevelProvider()),
-      ChangeNotifierProvider(create: (context) => ClassProvider()),
-      ChangeNotifierProvider(create: (_) => AssessmentProvider()),
-      ChangeNotifierProvider(create: (_) => TermProvider()),
-      ChangeNotifierProvider(create: (context) => CourseRegistrationProvider()),
-      ChangeNotifierProvider(create: (_) => StudentProvider()),
-    ],
-    child: const MyApp(),
-  ));
+
+  // Run the app
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthProvider()),
+        ChangeNotifierProvider(create: (context) => NewsProvider()),
+        ChangeNotifierProvider(create: (context) => SubjectProvider()),
+        ChangeNotifierProvider(create: (_) => CBTProvider(CBTService())),
+        ChangeNotifierProvider(create: (context) => GameProvider()),
+        ChangeNotifierProvider(create: (_) => ExamProvider()),
+        ChangeNotifierProvider(create: (context) => ForYouProvider()),
+        ChangeNotifierProvider(create: (context) => GradeProvider()),
+        ChangeNotifierProvider(create: (context) => LevelProvider()),
+        ChangeNotifierProvider(create: (context) => ClassProvider()),
+        ChangeNotifierProvider(create: (_) => AssessmentProvider()),
+        ChangeNotifierProvider(create: (_) => TermProvider()),
+        ChangeNotifierProvider(create: (context) => CourseRegistrationProvider()),
+        ChangeNotifierProvider(create: (_) => StudentProvider()),
+      ],
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
